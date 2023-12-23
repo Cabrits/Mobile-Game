285c5a9f7a86be190ab3cfad35dbf2dfc76df3cb
Duarte Cabrita <cabritagamer1@gmail.com>
2023-12-23 20:47:44 +0000

    Final Commit Atrasado
    Commit que inclui tudo o que foi apresentado no video

--- Assets/player.cs
+++ Assets/player.cs
@@ -3,58 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class player : MonoBehaviour
-{
+public class Player : MonoBehaviour{
 
     Rigidbody2D rb;
     public float moveSpeed;
     public float rotateAmount;
-    float rotate;
     int score;
 
     private void Awake(){
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButton(0)){
+    void Update(){
+        if (Input.GetMouseButton(0))
+        {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            if(mousePos.x < 0){
-                rotate = rotateAmount;
+            if (mousePos.x < 0)
+            {
+                transform.Rotate(0, 0, rotateAmount);
             }
-            else{
-                rotate = -rotateAmount;
+            else
+            {
+                transform.Rotate(0, 0, -rotateAmount);
             }
-
-            transform.Rotate(0, 0, rotate);
         }
+
+        WrapAround();
     }
 
     private void FixedUpdate(){
         rb.velocity = transform.up * moveSpeed;
     }
 
-    private void OnCollisionEnter2D(Collision2D colllision){
+    private void OnTriggerEnter2D(Collider2D other){
+    if (other.gameObject.tag == "Points"){
+        moveSpeed += 0.5f;
+        Destroy(other.gameObject);
+        score++;
 
-        if(colllision.gameObject.tag == "Points"){
-            Destroy(colllision.gameObject);
-            score++;
-
-            if(score >= 7){
-                print("Level Complete");
-            }
-        }
-        else if(colllision.gameObject.tag == "Lost"){
+        if (score >= 8){
             SceneManager.LoadScene("Game");
+            print("Level Complete");
         }
     }
+    else if (other.gameObject.tag == "Lost"){
+        SceneManager.LoadScene("Game");
+    }
+}
+
+    private void WrapAround(){
+    float screenLeft = Camera.main.ScreenToWorldPoint(Vector3.zero).x;
+    float screenRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
+    float screenTop = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0)).y;
+    float screenBottom = Camera.main.ScreenToWorldPoint(Vector3.zero).y;
+
+    Vector3 newPosition = transform.position;
+
+    if (transform.position.x < screenLeft){
+        newPosition.x = screenRight;
+    }
+    else if (transform.position.x > screenRight){
+        newPosition.x = screenLeft;
+    }
+
+    if (transform.position.y < screenBottom){
+        newPosition.y = screenTop;
+    }
+    else if (transform.position.y > screenTop){
+        newPosition.y = screenBottom;
+    }
+
+    transform.position = newPosition;
+}
 }
